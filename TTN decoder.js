3f263b4675c20cf41aec4e007243c6b1a20c110b
Thomas Wesenberg <53391037+thomaswesenberg@users.noreply.github.com>
2021-08-03 22:18:25 +0200

    Update regarding V3 (The Things Stack)

--- TTN decoder.js
+++ TTN decoder.js
@@ -1,3 +1,26 @@
+function decodeUplink(input) {
+  var data = {};
+  data.latitude = ((input.bytes[0]<<16)>>>0) + ((input.bytes[1]<<8)>>>0) + input.bytes[2];
+  data.latitude = (data.latitude / 16777215.0 * 180) - 90;
+  data.longitude = ((input.bytes[3]<<16)>>>0) + ((input.bytes[4]<<8)>>>0) + input.bytes[5];
+  data.longitude = (data.longitude / 16777215.0 * 360) - 180;
+  var altValue = ((input.bytes[6]<<8)>>>0) + input.bytes[7];
+  var sign = input.bytes[6] & (1 << 7);
+  if(sign) {
+    data.altitude = 0xFFFF0000 | altValue;
+  }
+  else {
+    data.altitude = altValue;
+  }
+  data.hdop = input.bytes[8] / 10.0;
+  data.option = input.bytes[9];
+  return {
+    data: data
+  };
+}
+
+
+--- old (V2) ---
 function Decoder(bytes, port) {
   // Decode an uplink message from a buffer of bytes to an object of fields.
   var decoded = {};
